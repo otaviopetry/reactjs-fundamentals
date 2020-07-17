d14fe6d5c564fcb67a3d592017dab3981fe456de
otaviopetry <otaviopetry@gmail.com>
2020-07-17 01:46:27 -0300

    chore: app working, failing tests
    Watching video solution, trying to figure it out

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,33 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    const file = uploadedFiles[0];
+
+    data.append('file', file.file, file.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.warn(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    // iterate trough array of files
+    files.forEach(file => {
+      const newFile = {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+      setUploadedFiles([...uploadedFiles, newFile]);
+    });
   }
 
   return (
